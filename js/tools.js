694adf03ee2ed44160a51727b78a6dff33299cab
Ramón Jiménez Pomareta <ramonjosejimenezpomareta@gmail.com>
2019-09-17 15:14:59 +0200

    bugfix: integer code values coming from integer optionSet, are replaced by its name

--- js/tools.js
+++ js/tools.js
@@ -1,12 +1,40 @@
     
+/** Translates optionSet codes into its names
+ * 
+ *  <optionSet code="OpS_YNU_Numeric_129" name="YNU_Numeric_129" id="mJKvd2WXQtp">
+ */ 
+function toBoolText_OpS_YNU_Numeric_129(text){
+    var value = String(text);
+    switch(value){
+        case "9":
+            return "Unknown";
+        case "2":
+            return "No";
+        case "1":
+            return "Yes";
+        default:
+            return booleanToYesNo(value);
+    }
+}
 
-function toBoolText(text){
-    switch(text){
+/** Translates optionSet codes into its names
+ * 
+ *  <optionSet code="OpS_Leish_SurvType" name="Leishmaniasis - Surveillance type" id="SqOSRZrBJzs">
+ */ 
+function toBoolText_OpS_Leish_SurvType(text){
+    var value = String(text);
+    switch(value){
         case "9":
             return "Unknown";
         case "8":
-            return WHEN_NON_APPLICABLE;
+            return "Non-applicable";
+        case "7":
+            return "Other";
+        case "2":
+            return "Integrated";
+        case "1":
+            return "Vertical";
         default:
-            return booleanToYesNo(text);
+            return text;
     }
 }
